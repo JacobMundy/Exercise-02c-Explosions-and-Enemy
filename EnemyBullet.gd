9524dd68b6d449f253a2b414f8797ec1ad625b06
JacobMundy <jmundy2003@gmail.com>
2022-09-16 15:13:22 -0400

    Added everything

--- EnemyBullet.gd
+++ EnemyBullet.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-
+ 
 var velocity = Vector2.ZERO
 var speed = 500.0
 var damage = 1
@@ -17,14 +17,15 @@ func _physics_process(_delta):
 	
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("damage"):
-		body.damage(damage)
-	Effects = get_node_or_null("/root/Game/Effects")
-	if Effects != null:
-		var explosion = Explosion.instance()
-		Effects.add_child(explosion)
-		explosion.global_position = global_position
-	queue_free()
+	if body.name == "Player":
+		if body.has_method("damage"):
+			body.damage(damage)
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instance()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+		queue_free()
 
 
 func _on_Timer_timeout():
